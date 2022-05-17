55c4a71c7a709148b39a08ac90d86b33e03a78a2
vinic <viniciusvvv2@gmail.com>
2022-05-17 11:34:14 -0300

    Commit inicial

---
+++ scripts/index.js
@@ -0,0 +1,49 @@
+function headerScroll() {
+  /*     if(document.getElementsByTagName('body')[0].offsetHeight - 15 < (window.innerHeight)){
+        document.getElementsByTagName('body')[0].style = 'width:100vw'
+    } */
+}
+headerScroll();
+
+function alerta() {
+  alert(0.1 + 0.2);
+  console.warn(1);
+}
+
+let contador = 1;
+document.getElementById("radio1").checked = true;
+
+function slider() {
+  contador++;
+  if (contador > 4) {
+    contador = 1;
+  }
+  document.getElementById(`radio${contador}`).checked = true;
+}
+
+setInterval(() => {
+  slider();
+}, 10000);
+
+
+
+//document.getElementsByTagName("aside")[1].innerHTML += '<img src="./assets/meia-estrela.png">'; 
+
+function addEstrela(count=0){
+  const est = document.getElementsByClassName("avaliacao-after");
+
+  for(let i=0; i<est.length;i++){
+    for(let e=0;e<count;e++){
+      est[i].innerHTML += '<img src="./assets/estrela.png">';
+    }
+  }
+
+  const arr = document.querySelectorAll(".avaliacao-after img");
+
+  for(let i=0;i<arr.length;i++){
+    arr[i].style = "height:16px; position:relative; top:1px;";
+  }
+}
+
+
+addEstrela(4)
